05d1f15deb09977567b07ac62a4a5f2f0602d654
Ning Liang <ningliang@cs.ucsb.edu>
2020-04-07 17:32:39 -0700

    lab01

--- rugfit2.cpp
+++ rugfit2.cpp
@@ -1,6 +1,6 @@
 // rugfit2.cpp - calculates fit of rug to a floor
 // YOUR NAME(S), TODAY'S DATE
-
+// Ning Liang, 2020/4/6
 #include <iostream>
 using namespace std;
 
@@ -10,14 +10,19 @@ class Rectangle {
 
 public:
     // DECLARE a parameterized constructor to set the width and length
-     
+       Rectangle(double width1=0, double length1=0);
+
     // DECLARE accessors to get the width and length
+       double getWidth();
 
+       double getLength();
+       
     // DECLARE mutators to set the width and lengtg
+       void setWidth(double a);
 
+       void setLength(double b);
     // DECLARE (const) METHOD TO RETURN AREA OF RECTANGLE
-    
-
+       double area() const;
 private:
     double width, length;
 
@@ -26,12 +31,29 @@ private:
 // implementation of class Rectangle here to main function
 // (usually would be in a separate file named rectangle.cpp,
 // and this separate file would #include "rectangle.h")
+Rectangle::Rectangle(double width1, double length1){
+	width = width1;
+	length = length1;
+}
 
 // IMPLEMENT CONSTRUCTOR USING INITIALIZATION LIST
-
+double Rectangle::getWidth(){
+	return width;
+}
+double Rectangle::getLength(){
+	return length;
+}
+void Rectangle::setWidth(double a){
+	width = a;
+}
+void Rectangle::setLength(double b){
+	length = b;
+}
 
 // IMPLEMENT ACCESSORS and MUTATORS
-
+double Rectangle::area() const{
+	return width * length;
+}
 
 // IMPLEMENT THE AREA METHOD HERE
 
@@ -51,7 +73,8 @@ int main() {
     const Rectangle floor(width, length); // note: const object
 
     // RESET Rectangle NAMED "rug" FOR SIZES INPUT BY USER
-    
+    rug.setWidth(0);
+    rug.setLength(0);
     
     
     // DO NOT CHANGE ANYTHING BELOW (if you do, you will earn 0 points,
